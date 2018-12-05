0ec299c31bf172ec71e6434508a2f968649715c7
Joycelind <joycelindchow@yahoo.co.id>
2018-12-05 17:27:39 +0800

    my first commit

---
+++ app/src/main/java/com/example/joycelindchow/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.joycelindchow.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View view) {
+        EditText editName = (EditText) findViewById(R.id.editText);
+        TextView textDisplay = (TextView) findViewById(R.id.textViewDisplay);
+        String nameToDisplay = editName.getText().toString();
+        textDisplay.setText("Hello " + nameToDisplay);
+        Log.d(TAG, nameToDisplay + ", this is your system log!");
+    }
+}
